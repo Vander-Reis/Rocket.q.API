024d43c686645969f8c7774c096b805fb22df2de
Vander-Reis <vanderreis2017@outlook.com>
2023-02-12 01:29:19 -0300

    create order by for questions

--- prisma/migrations/20230212042611_create_new_migrations/migration.sql
+++ prisma/migrations/20230212042611_create_new_migrations/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "room" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "room" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
@@ -11,6 +11,7 @@ CREATE TABLE "questions" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "description" TEXT NOT NULL,
     "read" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "questionId" TEXT NOT NULL,
     CONSTRAINT "questions_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "room" ("room") ON DELETE RESTRICT ON UPDATE CASCADE
 );
--- src/modules/question/useCases/listQuestions/ListQuestionsUseCase.ts
+++ src/modules/question/useCases/listQuestions/ListQuestionsUseCase.ts
@@ -6,11 +6,13 @@ interface IRequest {
 
 export class ListQuestionsUseCase {
   async execute({id}: IRequest) {
-    const question = await prisma.questions.findMany({
-       where: {
-        questionId: id
-       },
-    })
+    const question = await prisma.$queryRaw`
+      SELECT Q.*, Q.read, R.room
+	    FROM questions Q
+      INNER JOIN room R ON R.room = Q.questionId
+	    WHERE Q.questionId = ${id}
+      ORDER BY Q.read, Q.created_at;
+    `
 
     return question;
   }
